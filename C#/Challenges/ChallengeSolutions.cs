6bcef82bc5ea4738cc0560035f466c764714e22b
Xherdi Lika <dscherdil@gmail.com>
2022-10-05 20:22:29 +0200

    LongestPalindrome

---
+++ C#/Challenges/ChallengeSolutions.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Text;
+
+namespace TwoSum
+{
+    public static class ChallengeSolutions
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var map = new Dictionary<int, int>();
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (map.ContainsKey(target - nums[i]))
+                {
+                    return new int[] { i, map[target - nums[i]] };
+                }
+
+                map.TryAdd(nums[i], i);
+            }
+
+            return new int[] { 0, 1 };
+        }
+
+        public static string LongestPalindrome(string s)
+        {
+            Dictionary<char, int> memo = new();
+            var sA = new StringBuilder(s);
+            var longest = -1;
+            var lp = "" + sA[0];
+            var i = 0;
+            while (i < sA.Length)
+            {
+                if (!memo.ContainsKey(sA[i]))
+                {
+                    memo.Add(sA[i], i);
+                    i++;
+                }
+                else
+                {
+                    var (start, stop) = (memo[sA[i]], i);
+
+                    var len = stop - start + 1;
+                    if (len >= 4)
+                    {
+                        memo[sA[i]] = stop;
+                        i++;
+                        continue;
+                    }
+                    else if (len == 2)
+                    {
+                        var j = i + 1;
+                        while (j < sA.Length && sA[i] == sA[j])
+                        {
+                            stop++;
+                            j++;
+                        }
+
+                    } 
+                    memo[sA[i]] = stop;
+
+                    while (start - 1 > -1 && stop + 1 < sA.Length && sA[start - 1] == sA[stop + 1])
+                    {
+                        start--;
+                        stop++;
+                    }
+
+                    len = stop - start + 1;
+                    if (longest < len)
+                    {
+                        longest = len;
+                        lp = sA.ToString(start, len);
+                    }
+
+                        i++;
+                }
+            }
+
+            return lp;
+        }
+    }
+}
---
+++ C#/Challenges/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using TwoSum;
+
+// Console.WriteLine(JsonConvert.SerializeObject(ChallengeSolutions.TwoSum(new int[]{3,2,4}, 6)));
+Console.WriteLine(ChallengeSolutions.LongestPalindrome("aaabaaaa"));
+Console.WriteLine(ChallengeSolutions.LongestPalindrome("abacab"));
+Console.WriteLine(ChallengeSolutions.LongestPalindrome("abcdbbfcba"));
+Console.WriteLine(ChallengeSolutions.LongestPalindrome("ababababababa"));
