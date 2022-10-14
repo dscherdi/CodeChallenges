de8bd0589ba60bc3f3055d27d24c22d91681f222
Xherdi Lika <dscherdil@gmail.com>
2022-10-14 23:02:08 +0200

    Progress

--- C#/Challenges/ChallengeSolutions.cs
+++ C#/Challenges/ChallengeSolutions.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System;
-using System.Text;
+﻿using System.Text;
 
 namespace TwoSum
 {
@@ -56,8 +53,8 @@ namespace TwoSum
                             stop++;
                             j++;
                         }
+                    }
 
-                    } 
                     memo[sA[i]] = stop;
 
                     while (start - 1 > -1 && stop + 1 < sA.Length && sA[start - 1] == sA[stop + 1])
@@ -73,11 +70,104 @@ namespace TwoSum
                         lp = sA.ToString(start, len);
                     }
 
-                        i++;
+                    i++;
                 }
             }
 
             return lp;
+        }
+
+        public static bool IsMatch(string s, string p)
+        {
+            var sb = new StringBuilder(s);
+            var tokens = new List<string>();
+            var cp = p.ToCharArray();
+            for (var i = 0; i < cp.Length;)
+            {
+                if (i + 1 < cp.Length && cp[i + 1] == '*')
+                {
+                    tokens.Add(cp[i] + "" + cp[i + 1]);
+                    i += 2;
+                }
+                else
+                {
+                    tokens.Add("" + cp[i]);
+                    i++;
+                }
+            }
+            var j = 0;
+            foreach (var tok in tokens)
+            {
+                if (j >= sb.Length) return false;
+                var star = tok.Length == 2 && tok[1] == '*' ? true : false;
+                var cm = star ? "" + tok[0] : tok;
+                if (!star)
+                {
+                    if (cm != "." && tok != sb.ToString(j, cm.Length))
+                    {
+                        return false;
+                    }
+
+                    j++;
+                }
+                else
+                {
+                    while (j < sb.Length && (cm == "." || sb[j] + "" == cm))
+                    {
+                        j++;
+                    }
+                }
+            }
+
+            if (j < sb.Length)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool IsMatchDynamic(string s, string p)
+        {
+            var tokens = GetTokens(p);
+            return IsMatchDynamicHelper(s, tokens, 0, 0);
+        }
+        private static bool IsMatchDynamicHelper(string s, List<string> tokens, int i, int j)
+        {
+            for (;i < s.Length || j<tokens.Count; i++)
+            {
+                if (j<tokens.Count)
+                {
+                    if(tokens[j].ElementAtOrDefault(1) ==  '*') {
+                        if (i> s.Length || (tokens[j][0] != s.ElementAtOrDefault(i) && tokens[j][0] != '.')) return IsMatchDynamicHelper(s, tokens, i, j + 1);
+                    } else {
+                        if (tokens[j][0] == s.ElementAtOrDefault(i) || tokens[j][0] == '.') return IsMatchDynamicHelper(s, tokens, i + 1, j + 1);
+                        else return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private static List<string> GetTokens(string p)
+        {
+            var tokens = new List<string>();
+            var cp = p.ToCharArray();
+            for (var i = 0; i < cp.Length;)
+            {
+                if (i + 1 < cp.Length && cp[i + 1] == '*')
+                {
+                    tokens.Add(cp[i] + "" + cp[i + 1]);
+                    i += 2;
+                }
+                else
+                {
+                    tokens.Add("" + cp[i]);
+                    i++;
+                }
+            }
+            return tokens;
         }
     }
 }
--- C#/Challenges/Program.cs
+++ C#/Challenges/Program.cs
@@ -6,7 +6,11 @@ using Newtonsoft.Json;
 using TwoSum;
 
 // Console.WriteLine(JsonConvert.SerializeObject(ChallengeSolutions.TwoSum(new int[]{3,2,4}, 6)));
-Console.WriteLine(ChallengeSolutions.LongestPalindrome("aaabaaaa"));
-Console.WriteLine(ChallengeSolutions.LongestPalindrome("abacab"));
-Console.WriteLine(ChallengeSolutions.LongestPalindrome("abcdbbfcba"));
-Console.WriteLine(ChallengeSolutions.LongestPalindrome("ababababababa"));
+// Console.WriteLine(ChallengeSolutions.LongestPalindrome("aaabaaaa"));
+// Console.WriteLine(ChallengeSolutions.LongestPalindrome("abacab"));
+// Console.WriteLine(ChallengeSolutions.LongestPalindrome("abcdbbfcba"));
+// Console.WriteLine(ChallengeSolutions.LongestPalindrome("ababababababa"));
+Console.WriteLine(ChallengeSolutions.IsMatchDynamic("aabcadwdrgw", "a*b*"));
+Console.WriteLine(ChallengeSolutions.IsMatchDynamic("mississippi", "mis*is*ip*."));
+Console.WriteLine(ChallengeSolutions.IsMatchDynamic("ab", ".*c"));
+Console.WriteLine(ChallengeSolutions.IsMatchDynamic("aaa", "a*a"));
