b7b21a6acf32772476e95f55322c3a639d634f2a
dscherdil <dscherdil@gmail.com>
2022-12-03 11:36:00 +0100

    Climbing Staris

--- Go/challenges.solutions.go
+++ Go/challenges.solutions.go
@@ -269,6 +269,22 @@ func MaxProfit2(prices []int) int {
 	return maxReturn
 }
 
+// https://leetcode.com/problems/climbing-stairs/
+func ClimbStairs(n int) int {
+	defer timeTrack(time.Now(), "ClimbStairs")
+	if n < 3 {
+		return n
+	}
+
+	dp := make([]int, n)
+	dp[0] = 1
+	dp[1] = 2
+	for i := 2; i < n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n-1]
+}
+
 // Utils
 func FactorialUInt64(n uint64) uint64 {
 	var p uint64 = 1
