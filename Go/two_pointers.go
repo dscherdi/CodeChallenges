cd5496f324d88e683119c0dcabc4b84aaa48221a
Xherdi Lika <dscherdil@gmail.com>
2023-12-09 21:53:48 +0100

    More problems

--- Go/two_pointers.go
+++ Go/two_pointers.go
@@ -111,3 +111,185 @@ func threeSum(nums []int) [][]int {
 	}
 	return result
 }
+
+// https://leetcode.com/problems/minimum-size-subarray-sum/?envType=study-plan-v2&envId=top-interview-150
+func minSubArrayLen(target int, nums []int) int {
+	n := len(nums)
+	minLen := n + 1
+	sum := 0
+	start := 0
+
+	for end := 0; end < n; end++ {
+		sum += nums[end]
+
+		for sum >= target {
+			if end-start+1 < minLen {
+				minLen = end - start + 1
+			}
+			sum -= nums[start]
+			start++
+		}
+	}
+
+	if minLen == n+1 {
+		return 0
+	}
+	return minLen
+}
+
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/?envType=study-plan-v2&envId=top-interview-150
+func lengthOfLongestSubstring(s string) int {
+	table := make(map[byte]int)
+
+	maxLen := 0
+	start := 0
+
+	for end := 0; end < len(s); end++ {
+		char := s[end]
+		if _, ok := table[char]; ok {
+			start = int(math.Max(float64(start), float64(table[char]+1)))
+		}
+		table[char] = end
+		maxLen = int(math.Max(float64(maxLen), float64(end-start+1)))
+	}
+
+	return maxLen
+}
+
+// https://leetcode.com/problems/minimum-window-substring/?envType=study-plan-v2&envId=top-interview-150
+func minWindow(s string, t string) string {
+	if len(s) < len(t) {
+		return ""
+	}
+
+	if len(t) == 1 {
+		if strings.Contains(s, t) {
+			return t
+		}
+		return ""
+	}
+
+	table := make(map[byte]int)
+
+	for i := range t {
+		table[t[i]]++
+	}
+
+	start := 0
+	end := 0
+	minEnd := 0
+	minStart := 0
+	minLen := len(s) + 1
+
+	isValidTable := func() bool {
+		for _, v := range table {
+			if v > 0 {
+				return false
+			}
+		}
+		return true
+	}
+
+	for ; end < len(s); end++ {
+		if _, ok := table[s[end]]; ok {
+			table[s[end]]--
+		}
+
+		for isValidTable() {
+			if end-start+1 < minLen {
+				minLen = end - start + 1
+				minStart = start
+				minEnd = end
+			}
+			if _, ok := table[s[start]]; ok {
+				table[s[start]]++
+			}
+			start++
+		}
+
+	}
+
+	if minLen == len(s)+1 {
+		return ""
+	}
+
+	return s[minStart:minEnd]
+}
+
+// https://leetcode.com/problems/substring-with-concatenation-of-all-words/?envType=study-plan-v2&envId=top-interview-150
+func findSubstring(s string, words []string) []int {
+	table := make(map[string]int)
+
+	for _, word := range words {
+		table[word]++
+	}
+
+	result := []int{}
+	wordLen := len(words[0])
+	wordsLen := len(words)
+	totalLen := wordLen * wordsLen
+
+	for i := 0; i < len(s)-totalLen+1; i++ {
+		seen := make(map[string]int)
+		for j := 0; j < totalLen; j += wordLen {
+			word := s[i+j : i+j+wordLen]
+			if _, ok := table[word]; ok {
+				seen[word]++
+				if seen[word] > table[word] {
+					break
+				}
+			} else {
+				break
+			}
+		}
+		same := true
+		for k, v := range table {
+			if seen[k] != v {
+				same = false
+				break
+			}
+		}
+		if same {
+			result = append(result, i)
+		}
+	}
+
+	return result
+}
+
+// https://leetcode.com/problems/game-of-life/?envType=study-plan-v2&envId=top-interview-150
+func gameOfLife(board [][]int) {
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[i]); j++ {
+			alive := 0
+			for k := i - 1; k <= i+1; k++ {
+				for l := j - 1; l <= j+1; l++ {
+					if k >= 0 && k < len(board) && l >= 0 && l < len(board[i]) && board[k][l]%2 == 1 && (k != i || l != j) {
+						alive++
+					}
+
+				}
+			}
+			if board[i][j] == 1 {
+				if alive > 3 {
+					board[i][j] = 1
+				} else if alive == 2 || alive == 3 {
+					board[i][j] = 3
+				} else {
+					board[i][j] = 1
+				}
+			} else {
+				if alive == 3 {
+					board[i][j] = 2
+				}
+			}
+
+		}
+	}
+
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[i]); j++ {
+			board[i][j] /= 2
+		}
+	}
+}
