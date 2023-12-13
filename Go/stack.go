5e705569bcc4623e60cad4319be231be6f548cd2
Xherdi Lika <dscherdil@gmail.com>
2023-12-13 09:10:39 +0100

    More problems: RPN

--- Go/stack.go
+++ Go/stack.go
@@ -1,6 +1,9 @@
 package main
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
 // https://leetcode.com/problems/valid-parentheses/?envType=study-plan-v2&envId=top-interview-150
 func isValid(s string) bool {
@@ -41,4 +44,39 @@ func simplifyPath(path string) string {
 	correctPath := "/" + strings.Join(stack, "/")
 
 	return correctPath
+}
+
+// https://leetcode.com/problems/evaluate-reverse-polish-notation/?envType=study-plan-v2&envId=top-interview-150
+func evalRPN(tokens []string) int {
+	var stack []int
+
+	performOperation := func(op1 int, op2 int, operator string) int {
+		switch operator {
+		case "*":
+			return op1 * op2
+		case "/":
+			return op1 / op2
+		case "+":
+			return op1 + op2
+		case "-":
+			return op1 - op2
+		default:
+			panic("Bad operator")
+		}
+	}
+
+	for _, t := range tokens {
+		switch t {
+		case "*", "/", "+", "-":
+			operand1 := stack[len(stack)-2]
+			operand2 := stack[len(stack)-1]
+			stack = stack[:len(stack)-2]
+			stack = append(stack, performOperation(operand1, operand2, t))
+		default:
+			num, _ := strconv.Atoi(t)
+			stack = append(stack, num)
+		}
+	}
+
+	return stack[len(stack)-1]
 }
