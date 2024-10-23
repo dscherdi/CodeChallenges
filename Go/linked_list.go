e6d6e8a3e489d68ac0861c9b3c3f69582b96021a
Xherdi Lika <dscherdil@gmail.com>
2024-10-23 21:28:58 +0200

    Add new Problems

--- Go/linked_list.go
+++ Go/linked_list.go
@@ -18,3 +18,58 @@ func hasCycle(head *ListNode) bool {
 
 	return true
 }
+
+// https://leetcode.com/problems/add-two-numbers/?envType=study-plan-v2&envId=top-interview-150
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var result *ListNode = nil
+	previousElement := result
+	head := result
+	carry := 0
+
+	for l1.Next != nil && l2.Next != nil {
+
+		sum := l1.Val + l2.Val + carry
+		carry = sum / 10
+
+		newElement := &ListNode{Val: sum % 10}
+		previousElement = head
+		head = newElement
+		previousElement.Next = head
+
+		l1 = l1.Next
+		l2 = l2.Next
+	}
+
+	return result
+}
+
+// https://leetcode.com/problems/merge-two-sorted-lists/?envType=study-plan-v2&envId=top-interview-150
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	var result *ListNode
+
+	var previous *ListNode
+	var head *ListNode
+
+	for list1 != nil || list2 != nil {
+
+		previous = head
+
+		if list1 == nil || (list2 != nil && list2.Val <= list1.Val) {
+			head = list2
+			list2 = list2.Next
+		} else if list2 == nil || list1.Val < list2.Val {
+			head = list1
+			list1 = list1.Next
+		}
+
+		if previous != nil {
+			previous.Next = head
+		}
+
+		if result == nil {
+			result = head
+		}
+	}
+
+	return result
+}
--- Go/linked_list_test.go
+++ Go/linked_list_test.go
@@ -28,3 +28,71 @@ func TestHasCycle(t *testing.T) {
 		t.Errorf("hasCycle was incorrect, got: true, want: false.")
 	}
 }
+
+func TestAddTwoNumbers(t *testing.T) {
+	// Create two linked lists of equal length
+	node1 := &ListNode{Val: 2}
+	node2 := &ListNode{Val: 4}
+	node3 := &ListNode{Val: 3}
+	node1.Next = node2
+	node2.Next = node3
+
+	node4 := &ListNode{Val: 5}
+	node5 := &ListNode{Val: 6}
+	node6 := &ListNode{Val: 4}
+	node4.Next = node5
+	node5.Next = node6
+
+	result := addTwoNumbers(node1, node4)
+	if result.Val != 7 || result.Next.Val != 0 || result.Next.Next.Val != 8 {
+		t.Errorf("addTwoNumbers was incorrect, got: %d%d%d, want: 708.", result.Val, result.Next.Val, result.Next.Next.Val)
+	}
+
+	// Create two linked lists of unequal length
+	node7 := &ListNode{Val: 1}
+	node8 := &ListNode{Val: 8}
+	node7.Next = node8
+
+	node9 := &ListNode{Val: 0}
+
+	result = addTwoNumbers(node7, node9)
+	if result.Val != 1 || result.Next.Val != 8 {
+		t.Errorf("addTwoNumbers was incorrect, got: %d%d, want: 18.", result.Val, result.Next.Val)
+	}
+}
+
+func TestMergeTwoLists(t *testing.T) {
+	// Create two linked lists of equal length
+	node1 := &ListNode{Val: 1}
+	node2 := &ListNode{Val: 2}
+	node3 := &ListNode{Val: 4}
+	node1.Next = node2
+	node2.Next = node3
+
+	node4 := &ListNode{Val: 1}
+	node5 := &ListNode{Val: 3}
+	node6 := &ListNode{Val: 4}
+	node4.Next = node5
+	node5.Next = node6
+
+	result := mergeTwoLists(node1, node4)
+	if result.Val != 1 || result.Next.Val != 1 || result.Next.Next.Val != 2 || result.Next.Next.Next.Val != 3 || result.Next.Next.Next.Next.Val != 4 || result.Next.Next.Next.Next.Next.Val != 4 {
+		t.Errorf("mergeTwoLists was incorrect, got: %d%d%d%d%d%d, want: 112344.", result.Val, result.Next.Val, result.Next.Next.Val, result.Next.Next.Next.Val, result.Next.Next.Next.Next.Val, result.Next.Next.Next.Next.Next.Val)
+	}
+
+	// Create two linked lists of unequal length
+	node7 := &ListNode{Val: 1}
+	node8 := &ListNode{Val: 2}
+	node7.Next = node8
+
+	node9 := &ListNode{Val: 3}
+	node10 := &ListNode{Val: 4}
+	node11 := &ListNode{Val: 5}
+	node9.Next = node10
+	node10.Next = node11
+
+	result = mergeTwoLists(node7, node9)
+	if result.Val != 1 || result.Next.Val != 2 || result.Next.Next.Val != 3 || result.Next.Next.Next.Val != 4 || result.Next.Next.Next.Next.Val != 5 {
+		t.Errorf("mergeTwoLists was incorrect, got: %d%d%d%d%d, want: 12345.", result.Val, result.Next.Val, result.Next.Next.Val, result.Next.Next.Next.Val, result.Next.Next.Next.Next.Val)
+	}
+}
